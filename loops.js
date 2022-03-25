f1d71b6c72c68ece3f2680dc7e39c92eb6cd51b4
MitsGeest <mitchelgeest@gmail.com>
2022-03-25 07:47:28 +0100

    Opdracht af

--- loops.js
+++ loops.js
@@ -6,7 +6,11 @@
 // Hoera!
 // ==========================================
 
+const felicitatie = "Hoera!";
 
+for (let i = 0; i < 3; i++) {
+    console.log(felicitatie);
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -18,7 +22,22 @@
 // klaar!
  // ==========================================
 
+//STAPPENPLAN
+// 1. Declareer variabelen die als uitkomst van de loop dienen
+// 2. Maak een for-loop die vijf uitkomsten geeft
+// 3. Bepaal dat de eerste vier uitkomsten "loop..." geven
+// 4. Bepaal dat de laatste uitkomst "klaar!" geeft.
 
+const firstLoop = "loop..";
+const endLoop = "klaar!";
+
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log(firstLoop);
+    } else {
+        console.log(endLoop);
+    }
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -33,7 +52,22 @@
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
 
+//STAPPENPLAN
+// 1. Maak een for loop die van 0 tot 15 loopt
+// 2. Bepaal welke uitkomsten even zijn en welke uitkomsten oneven zijn door gebruik te maken van de remainder operator
+// 3. Bepaal de variabelen die onderdeel zijn van de console.log
+// 4. Formuleer de uitkomsten voor de even en de oneven optie
 
+const evenOutcome = " is even";
+const unevenOutcome = " is oneven";
+
+for (let i = 0; i < 16; i++) {
+    if (i % 2 === 0) {
+        console.log(i + evenOutcome);
+    } else {
+        console.log(i + unevenOutcome);
+    }
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -51,3 +85,12 @@
 // >> 9
 // ==========================================
 
+for (let i=0; i<10; i++) {
+    if (i < 4) {
+        console.log(i);
+    } else if (i < 7) {
+        console.log("> " + i);
+    } else {
+        console.log (">> " + i);
+    }
+}
